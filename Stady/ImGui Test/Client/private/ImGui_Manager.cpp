6a1f9d88e32691d5bc1bda0dc46e5a3630ab2008
SuHyeonOk <tntngus1223@naver.com>
2022-11-09 00:57:34 +0900

    Imgui study

--- Stady/ImGui Test/Client/private/ImGui_Manager.cpp
+++ Stady/ImGui Test/Client/private/ImGui_Manager.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "..\public\ImGui_Manager.h"
 
+#include <fstream>
+#include "../Default/ImGui/ImGuiFileDialog.h"
+
 IMPLEMENT_SINGLETON(CImGui_Manager)
 
 CImGui_Manager::CImGui_Manager()
@@ -55,7 +58,7 @@ void CImGui_Manager::Tick_Imgui()
 
 	ImGui::Begin("Info Tool");
 	Info_Tool();
-	ImGui::End(); // 창에 필요한 기능을 다 넣었다면 End로 닫아줘야함
+	ImGui::End(); // 창에 필요한 기능을 다 넣었다면 End로 닫기
 }
 
 void CImGui_Manager::Render_Imgui()
@@ -73,46 +76,94 @@ void CImGui_Manager::Render_Imgui()
 
 void CImGui_Manager::Info_Tool()
 {
-	static _bool bCheckbox = false;
-	static _float fSliderFloat = 0.f;
-	static _float fColorEdit3 = 0.f;
-	static _int iButtonUp = 0;
+	// TODO : int형 변수 하나를 텍스처 파일에 저장하기
 
+	static _int		iHp = 0;
 
-	ImGui::Checkbox("Checkbox", &bCheckbox);
-	ImGui::SliderFloat("float", &fSliderFloat, 0.0f, 1.0f);
-	ImGui::ColorEdit3("dd", &fColorEdit3);
-	if (ImGui::Button("Button"))
-		iButtonUp++;
+	ImGui::SliderInt("input HP", &iHp, 0, 100);
+	ImGui::Text("HP : %d", iHp);
 
-	//ImGui::SameLine(); // 다음 것 이어서
-	ImGui::Text("bCheckbox = %o", bCheckbox);
-	ImGui::Text("fSliderFloat = %f", fSliderFloat);
-	ImGui::Text("fColorEdit3 = %f", fColorEdit3);
-	ImGui::Text("iButtonUp = %d", iButtonUp);
-
-	// demo
-	static _bool	show_demo_window = false;
-	static _int		m_iTemp = 0;
-	if (ImGui::Button("show_demo_window") && m_iTemp == 0)
+	if (ImGui::Button("Open"))
 	{
-		show_demo_window = true;
-		++m_iTemp;
-	}
-	else if(m_iTemp == 2)
-	{
-		show_demo_window = false;
-		m_iTemp = 0;
-	}
-	ImGui::Text("m_iTemp = %d", m_iTemp);
+		ofstream ofs;
+		ofs.open("../../Data/Info.txt");
 
-	if (show_demo_window)
-		ImGui::ShowDemoWindow(&show_demo_window);
-	else
-		ImGui::ShowAboutWindow(&show_demo_window);
+		ofs << iHp << endl;
+
+
+		//std::ifstream ifs;
+		//ifs.open("../../Data/Info.txt");
+	}
+	if (ImGui::Button("Save"))
+	{
+		ofstream ofs("../../Data/Info.txt");
+		if (ofs.fail())
+		{
+			MSG_BOX("Data File Error!");
+			return;
+		}
+		
+		list<_float3>			m_MonsterPos;
+		// TODO
+
+
+		ofs << iHp << endl;
+
+		return;
+	}
+
+	if (ImGui::Button("Save"))
+	{
+
+		ImGuiFileDialog::Instance()->OpenDialog("Test", "Test Test", nullptr, ".", 1, nullptr, ImGuiFileDialogFlags_None);
+	}
+
+
+
+	//static _bool bCheckbox = false;
+	//static _float fSliderFloat = 0.f;
+	//static _float fColorEdit3 = 0.f;
+	//static _int iButtonUp = 0;
+
+	//ImGui::Checkbox("Checkbox", &bCheckbox);
+	//ImGui::SliderFloat("float", &fSliderFloat, 0.0f, 1.0f);
+	//ImGui::ColorEdit3("dd", &fColorEdit3);
+	//if (ImGui::Button("Button"))
+	//	iButtonUp++;
+
+	//static wstring	sName = L"Ok";
+	//static _int		iLevel = 0;
+	//static _int		iHp = 100;
+	//static _int		iAttack = 5;
+
+	////ImGui::SameLine(); // 다음 것 이어서
+	//ImGui::Text("bCheckbox = %o", bCheckbox);
+	//ImGui::Text("fSliderFloat = %f", fSliderFloat);
+	//ImGui::Text("fColorEdit3 = %f", fColorEdit3);
+	//ImGui::Text("iButtonUp = %d", iButtonUp);
+
+	//// demo
+	//static _bool	show_demo_window = false;
+	//static _int		m_iTemp = 0;
+	//if (ImGui::Button("show_demo_window") && m_iTemp == 0)
+	//{
+	//	show_demo_window = true;
+	//	++m_iTemp;
+	//}
+	//else if(m_iTemp == 2)
+	//{
+	//	show_demo_window = false;
+	//	m_iTemp = 0;
+	//}
+	//ImGui::Text("m_iTemp = %d", m_iTemp);
+
+	//if (show_demo_window)
+	//	ImGui::ShowDemoWindow(&show_demo_window);
+	//else
+	//	ImGui::ShowAboutWindow(&show_demo_window);
 		
 
-	//ImGui::ShowDebugLogWindow(&show_demo_window); // MousePos값알 수 있음
+	//ImGui::ShowDebugLogWindow(&show_demo_window); // MousePos값알 수 있던데 '-'
 
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 }
