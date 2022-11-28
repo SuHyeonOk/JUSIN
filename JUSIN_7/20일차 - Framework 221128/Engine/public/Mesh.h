a95ff25eeb630fbda8d33a4fe83a40714f9dc3fe
SuHyeonOk <tntngus1223@naver.com>
2022-11-28 22:37:10 +0900

    7_20 Texturing a Model, an animation theory of animation

---
+++ JUSIN_7/20일차 - Framework 221128/Engine/public/Mesh.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "VIBuffer.h"
+#include "Model.h"
+
+/* 모델의 교체가능한 한 파츠. */
+/* 이 메시를 그려내기위한 정점, 인덱스 버퍼를 보유한다. */
+BEGIN(Engine)
+
+class CMesh final : public CVIBuffer
+{
+private:
+	CMesh(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CMesh(const CMesh& rhs);
+	virtual ~CMesh() = default;
+
+public:
+	_uint Get_MaterialIndex() const {
+		return m_iMaterialIndex;
+	}
+
+public:
+	virtual HRESULT Initialize_Prototype(CModel::TYPE eType, aiMesh* pAIMesh);
+	virtual HRESULT Initialize(void* pArg) override;
+
+private:
+	CModel::TYPE		m_eType;
+	/* 이 메시는 m_iMaterialIndex번째 머테리얼을 사용한다. */
+	_uint				m_iMaterialIndex = 0;
+
+	/* 이 메시의 정점들에게 영향을 주는 뼈의 갯수. */
+	_uint				m_iNumBones = 0;
+
+private:
+	HRESULT Ready_VertexBuffer_NonAnimModel(aiMesh* pAIMesh);
+	HRESULT Ready_VertexBuffer_AnimModel(aiMesh* pAIMesh);
+
+public:
+	static CMesh* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, CModel::TYPE eType, aiMesh* pAIMesh);
+	virtual CComponent* Clone(void* pArg = nullptr) override;
+	virtual void Free();
+};
+
+END
---
+++ JUSIN_7/20일차 - Framework 221128/Engine/public/Model.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Component.h"
+
+BEGIN(Engine)
+
+class ENGINE_DLL CModel final : public CComponent
+{
+public:
+	enum TYPE { TYPE_NONANIM, TYPE_ANIM, TYPE_END };
+private:
+	CModel(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CModel(const CModel& rhs);
+	virtual ~CModel() = default;
+
+public:
+	_uint Get_NumMeshes() const {
+		return m_iNumMeshes;
+	}
+
+public:
+	virtual HRESULT Initialize_Prototype(TYPE eType, const char* pModelFilePath);
+	virtual HRESULT Initialize(void* pArg);
+
+public:
+	HRESULT Bind_Material(class CShader* pShader, _uint iMeshIndex, aiTextureType eType, const char* pConstantName);
+	HRESULT Render(CShader* pShader, _uint iMeshIndex);
+
+public:
+	const aiScene*						m_pAIScene = nullptr;
+	Assimp::Importer					m_Importer;
+	TYPE								m_eType = TYPE_END;
+
+	/* 하나의 모델은 교체가 가능한 여러개의 메시로 구성되어있다. */
+	_uint								m_iNumMeshes = 0; // 몇 개의 메시로 구성되어 있는지
+	vector<class CMesh*>				m_Meshes;
+
+	_uint								m_iNumMaterials = 0; // 재질 여러개
+	vector<MODELMATERIAL>				m_Materials;
+
+public:
+	HRESULT Ready_MeshContainers();
+	HRESULT Ready_Materials(const char* pModelFilePath);
+
+public:
+	static CModel* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, TYPE eType, const char* pModelFilePath);
+	virtual CComponent* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+
+END
