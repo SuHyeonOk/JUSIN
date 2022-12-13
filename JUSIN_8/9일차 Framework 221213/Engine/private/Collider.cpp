92db1392f29fb53416d95204927be1ad7d153755
SuHyeonOk <tntngus1223@naver.com>
2022-12-13 16:50:55 +0900

    8_9 Put the inorganic model player's hand message

---
+++ JUSIN_8/9일차 Framework 221213/Engine/private/Collider.cpp
@@ -0,0 +1,404 @@
+#include "..\public\Collider.h"
+#include "DebugDraw.h"
+
+#include "PipeLine.h"
+
+CCollider::CCollider(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CComponent(pDevice, pContext)
+{
+
+}
+
+CCollider::CCollider(const CCollider & rhs)
+	: CComponent(rhs)
+	, m_eType(rhs.m_eType)
+#ifdef _DEBUG
+	, m_pBatch(rhs.m_pBatch)
+	, m_pEffect(rhs.m_pEffect)
+	, m_pInputLayout(rhs.m_pInputLayout)
+#endif // _DEBUG
+{
+#ifdef _DEBUG
+	Safe_AddRef(m_pInputLayout);
+#endif // _DEBUG
+
+}
+
+HRESULT CCollider::Initialize_Prototype(TYPE eType)
+{
+	m_eType = eType;
+
+#ifdef _DEBUG
+
+	m_pBatch = new PrimitiveBatch<VertexPositionColor>(m_pContext);
+
+	m_pEffect = new BasicEffect(m_pDevice);
+	m_pEffect->SetVertexColorEnabled(true);
+
+	const void*		pShaderByteCode;
+	size_t			iShaderByteCodeSize;
+
+	m_pEffect->GetVertexShaderBytecode(&pShaderByteCode, &iShaderByteCodeSize);
+
+	if (FAILED(m_pDevice->CreateInputLayout(VertexPositionColor::InputElements, VertexPositionColor::InputElementCount, pShaderByteCode, iShaderByteCodeSize, &m_pInputLayout)))
+		return E_FAIL;
+
+#endif
+
+	return S_OK;
+}
+
+HRESULT CCollider::Initialize(void * pArg)
+{
+	COLLIDERDESC		ColliderDesc;
+	memcpy(&ColliderDesc, pArg, sizeof(COLLIDERDESC));	
+
+	switch (m_eType)
+	{
+	case CCollider::TYPE_AABB:
+		m_pAABB_Original = new BoundingBox(_float3(0.f, 0.f, 0.f), _float3(0.5f, 0.5f, 0.5f));
+		m_pAABB_Original->Transform(*m_pAABB_Original,
+			XMMatrixScaling(ColliderDesc.vSize.x, ColliderDesc.vSize.y, ColliderDesc.vSize.z) *
+			XMMatrixTranslation(ColliderDesc.vCenter.x, ColliderDesc.vCenter.y, ColliderDesc.vCenter.z));		
+		m_pAABB = new BoundingBox(*m_pAABB_Original);
+
+		break;
+
+	case CCollider::TYPE_OBB:
+		m_pOBB_Original = new BoundingOrientedBox(_float3(0.f, 0.f, 0.f), _float3(ColliderDesc.vSize.x * 0.5f, ColliderDesc.vSize.y * 0.5f, ColliderDesc.vSize.z * 0.5f), _float4(0.f, 0.f, 0.f, 1.f));
+		m_pOBB_Original->Transform(*m_pOBB_Original,
+			// XMMatrixScaling(ColliderDesc.vSize.x, ColliderDesc.vSize.y, ColliderDesc.vSize.z) *			
+			XMMatrixRotationX(ColliderDesc.vRotation.x) *
+			XMMatrixRotationY(ColliderDesc.vRotation.y) *
+			XMMatrixRotationZ(ColliderDesc.vRotation.z) *
+			XMMatrixTranslation(ColliderDesc.vCenter.x, ColliderDesc.vCenter.y, ColliderDesc.vCenter.z));
+		m_pOBB = new BoundingOrientedBox(*m_pOBB_Original);
+		break;
+
+	case CCollider::TYPE_SPHERE:
+		m_pSphere_Original = new BoundingSphere(_float3(0.f, 0.f, 0.f), 0.5f);
+
+		m_pSphere_Original->Transform(*m_pSphere_Original,
+			XMMatrixScaling(ColliderDesc.vSize.x, ColliderDesc.vSize.y, ColliderDesc.vSize.z) *
+			XMMatrixRotationX(ColliderDesc.vRotation.x) *
+			XMMatrixRotationY(ColliderDesc.vRotation.y) *
+			XMMatrixRotationZ(ColliderDesc.vRotation.z) *
+			XMMatrixTranslation(ColliderDesc.vCenter.x, ColliderDesc.vCenter.y, ColliderDesc.vCenter.z));
+
+		m_pSphere = new BoundingSphere(*m_pSphere_Original);
+		break;
+	}
+	
+	return S_OK;
+}
+void CCollider::Update(_fmatrix TransformMatrix)
+{
+	switch (m_eType)
+	{
+	case CCollider::TYPE_AABB:
+		m_pAABB_Original->Transform(*m_pAABB, Remove_Rotation(TransformMatrix));
+		break;
+
+	case CCollider::TYPE_OBB:
+		m_pOBB_Original->Transform(*m_pOBB, TransformMatrix);
+		break;
+
+	case CCollider::TYPE_SPHERE:
+		m_pSphere_Original->Transform(*m_pSphere, TransformMatrix);
+		break;
+	}
+}
+
+_bool CCollider::Collision(CCollider * pTargetCollider) 
+{
+	m_isColl = false;
+
+	switch (m_eType)
+	{
+	case CCollider::TYPE_AABB:
+		if (TYPE_AABB == pTargetCollider->m_eType)
+			m_isColl = m_pAABB->Intersects(*pTargetCollider->m_pAABB);
+		if (TYPE_OBB == pTargetCollider->m_eType)
+			m_isColl = m_pAABB->Intersects(*pTargetCollider->m_pOBB);
+		if (TYPE_SPHERE == pTargetCollider->m_eType)
+			m_isColl = m_pAABB->Intersects(*pTargetCollider->m_pSphere);
+		break;
+	case CCollider::TYPE_OBB:
+		if (TYPE_AABB == pTargetCollider->m_eType)
+			m_isColl = m_pOBB->Intersects(*pTargetCollider->m_pAABB);
+		if (TYPE_OBB == pTargetCollider->m_eType)
+			m_isColl = m_pOBB->Intersects(*pTargetCollider->m_pOBB);
+		if (TYPE_SPHERE == pTargetCollider->m_eType)
+			m_isColl = m_pOBB->Intersects(*pTargetCollider->m_pSphere);
+		break;
+	case CCollider::TYPE_SPHERE:
+		if (TYPE_AABB == pTargetCollider->m_eType)
+			m_isColl = m_pSphere->Intersects(*pTargetCollider->m_pAABB);
+		if (TYPE_OBB == pTargetCollider->m_eType)
+			m_isColl = m_pSphere->Intersects(*pTargetCollider->m_pOBB);
+		if (TYPE_SPHERE == pTargetCollider->m_eType)
+			m_isColl = m_pSphere->Intersects(*pTargetCollider->m_pSphere);
+		break;
+	}
+
+	return m_isColl;
+}
+
+_bool CCollider::Collision_AABB(CCollider * pTargetCollider)
+{
+	if (TYPE_AABB != m_eType ||
+		TYPE_AABB != pTargetCollider->m_eType)
+		return false;
+
+	_float3			vSourMin, vSourMax;
+	_float3			vDestMin, vDestMax;
+
+	vSourMin = Compute_Min();
+	vSourMax = Compute_Max();
+	vDestMin = pTargetCollider->Compute_Min();
+	vDestMax = pTargetCollider->Compute_Max();
+
+	m_isColl = false;
+
+	/* x축 선상에서 비교. */
+	if (max(vSourMin.x, vDestMin.x) > min(vSourMax.x, vDestMax.x))
+		return m_isColl;
+
+	/* y축 선상에서 비교. */
+	if (max(vSourMin.y, vDestMin.y) > min(vSourMax.y, vDestMax.y))
+		return m_isColl;
+
+	/* z축 선상에서 비교. */
+	if (max(vSourMin.z, vDestMin.z) > min(vSourMax.z, vDestMax.z))
+		return m_isColl;
+
+	m_isColl = true;
+
+	return m_isColl;
+}
+
+_bool CCollider::Collision_OBB(CCollider * pTargetCollider)
+{
+	if (TYPE_OBB != m_eType ||
+		TYPE_OBB != pTargetCollider->m_eType)
+		return false;
+
+	m_isColl = true;
+
+	OBBDESC		OBBDesc[2];
+
+	OBBDesc[0] = Compute_OBBDesc();
+	OBBDesc[1] = pTargetCollider->Compute_OBBDesc();
+
+	/* 0 : 두 박스의 센터를 잇는 벡터를 투영시킨 길이.  */
+	/* 1 : 첫번째 박스의 CenterAxis세개를 투영시킨 길이의 합.  */
+	/* 2 : 두번째 박스의 CenterAxis세개를 투영시킨 길이의 합.  */
+	_float			fDistance[3]; 
+
+	for (_uint i = 0; i < 2; ++i)
+	{
+		for (_uint j = 0; j < 3; ++j)
+		{
+			// 0
+			fDistance[0] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenter) - XMLoadFloat3(&OBBDesc[0].vCenter), 
+				XMLoadFloat3(&OBBDesc[i].vAlignAxis[j]))));
+
+			// 1
+			fDistance[1] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[0]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[j])))) +
+				fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[1]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[j])))) +
+				fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[2]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[j]))));
+
+			// 2
+			fDistance[2] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[0]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[0])))) +
+				fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[1]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[0])))) +
+				fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[2]),
+					XMLoadFloat3(&OBBDesc[i].vAlignAxis[0]))));
+
+			if (fDistance[0] > fDistance[1] + fDistance[2])
+			{
+				m_isColl = false;
+				return m_isColl;
+			}
+		}
+	}
+
+	_vector			vAlignAxis[9];
+
+	for (_uint i = 0; i < 3; ++i)
+	{
+		for (_uint j = 0; j < 3; ++j)
+		{
+			_uint		iIndex = i * 3 + j;
+
+			vAlignAxis[iIndex] = XMVector3Cross(XMLoadFloat3(&OBBDesc[0].vAlignAxis[i]), XMLoadFloat3(&OBBDesc[1].vAlignAxis[j]));
+		}
+	}
+	
+	for (_uint i = 0; i < 9; ++i)
+	{
+		fDistance[0] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenter) - XMLoadFloat3(&OBBDesc[0].vCenter),
+			vAlignAxis[i])));
+
+		fDistance[1] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[0]),
+				vAlignAxis[i]))) +
+			fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[1]),
+				vAlignAxis[i]))) +
+			fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[0].vCenterAxis[2]),
+				vAlignAxis[i])));
+
+		fDistance[2] = fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[0]),
+				vAlignAxis[i]))) +
+			fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[1]),
+				vAlignAxis[i]))) +			
+			fabs(XMVectorGetX(XMVector3Dot(XMLoadFloat3(&OBBDesc[1].vCenterAxis[2]),
+				vAlignAxis[i])));
+
+		if (fDistance[0] > fDistance[1] + fDistance[2])
+		{
+			m_isColl = false;
+			return m_isColl;
+		}
+	}
+	
+	return m_isColl;
+}
+
+#ifdef _DEBUG
+
+HRESULT CCollider::Render()
+{	
+	m_vColor = m_isColl == true ? _float4(1.f, 0.f, 0.f, 1.f) : _float4(0.f, 1.f, 0.f, 1.f);
+
+	m_pEffect->SetWorld(XMMatrixIdentity());
+
+	CPipeLine*		pPipeLine = GET_INSTANCE(CPipeLine);
+
+	m_pEffect->SetView(pPipeLine->Get_TransformMatrix(CPipeLine::D3DTS_VIEW));
+	m_pEffect->SetProjection(pPipeLine->Get_TransformMatrix(CPipeLine::D3DTS_PROJ));
+
+	RELEASE_INSTANCE(CPipeLine);
+
+	m_pEffect->Apply(m_pContext);
+
+	m_pContext->IASetInputLayout(m_pInputLayout);
+
+	m_pBatch->Begin();
+
+	switch (m_eType)
+	{
+	case CCollider::TYPE_AABB:
+		DX::Draw(m_pBatch, *m_pAABB, XMLoadFloat4(&m_vColor));
+		break;
+	case CCollider::TYPE_OBB:
+		DX::Draw(m_pBatch, *m_pOBB, XMLoadFloat4(&m_vColor));
+		break;
+	case CCollider::TYPE_SPHERE:
+		DX::Draw(m_pBatch, *m_pSphere, XMLoadFloat4(&m_vColor));
+		break;
+	}
+
+	m_pBatch->End();
+
+	return S_OK;
+}
+#endif // _DEBUG
+
+_matrix CCollider::Remove_Rotation(_fmatrix TransformMatrix)
+{
+	_matrix			ResultMatrix = TransformMatrix;
+
+	ResultMatrix.r[0] = XMVectorSet(1.f, 0.f, 0.f, 0.f) * XMVectorGetX(XMVector3Length(TransformMatrix.r[0]));
+	ResultMatrix.r[1] = XMVectorSet(0.f, 1.f, 0.f, 0.f) * XMVectorGetX(XMVector3Length(TransformMatrix.r[1]));
+	ResultMatrix.r[2] = XMVectorSet(0.f, 0.f, 1.f, 0.f) * XMVectorGetX(XMVector3Length(TransformMatrix.r[2]));
+
+	return ResultMatrix;
+}
+
+_float3 CCollider::Compute_Min()
+{
+	return _float3(m_pAABB->Center.x - m_pAABB->Extents.x, 
+		m_pAABB->Center.y - m_pAABB->Extents.y, 
+		m_pAABB->Center.z - m_pAABB->Extents.z);
+}
+
+_float3 CCollider::Compute_Max()
+{
+	return _float3(m_pAABB->Center.x + m_pAABB->Extents.x,
+		m_pAABB->Center.y + m_pAABB->Extents.y,
+		m_pAABB->Center.z + m_pAABB->Extents.z);
+}
+
+CCollider::OBBDESC CCollider::Compute_OBBDesc()
+{
+	OBBDESC			OBBDesc;
+	ZeroMemory(&OBBDesc, sizeof OBBDesc);
+
+	OBBDesc.vCenter = m_pOBB->Center;
+
+	_float3		vCorner[8];
+
+	m_pOBB->GetCorners(vCorner);
+
+	XMStoreFloat3(&OBBDesc.vCenterAxis[0], (XMLoadFloat3(&vCorner[2]) - XMLoadFloat3(&vCorner[3])) * 0.5f);
+	XMStoreFloat3(&OBBDesc.vCenterAxis[1], (XMLoadFloat3(&vCorner[2]) - XMLoadFloat3(&vCorner[1])) * 0.5f);
+	XMStoreFloat3(&OBBDesc.vCenterAxis[2], (XMLoadFloat3(&vCorner[2]) - XMLoadFloat3(&vCorner[6])) * 0.5f);
+
+	XMStoreFloat3(&OBBDesc.vAlignAxis[0], XMVector3Normalize(XMLoadFloat3(&OBBDesc.vCenterAxis[0])));
+	XMStoreFloat3(&OBBDesc.vAlignAxis[1], XMVector3Normalize(XMLoadFloat3(&OBBDesc.vCenterAxis[1])));
+	XMStoreFloat3(&OBBDesc.vAlignAxis[2], XMVector3Normalize(XMLoadFloat3(&OBBDesc.vCenterAxis[2])));
+
+	return OBBDesc;	
+}
+
+CCollider * CCollider::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext, TYPE eType)
+{
+	CCollider*		pInstance = new CCollider(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype(eType)))
+	{
+		MSG_BOX("Failed to Created : CCollider");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+CComponent * CCollider::Clone(void * pArg)
+{
+	CCollider*		pInstance = new CCollider(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Created : CCollider");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CCollider::Free()
+{
+	__super::Free();
+
+	Safe_Delete(m_pAABB_Original);
+	Safe_Delete(m_pAABB);
+	Safe_Delete(m_pOBB_Original);
+	Safe_Delete(m_pOBB);
+	Safe_Delete(m_pSphere_Original);
+	Safe_Delete(m_pSphere);
+
+#ifdef _DEBUG
+
+	Safe_Release(m_pInputLayout);
+
+	if (false == m_isCloned)
+	{
+		Safe_Delete(m_pBatch);
+		Safe_Delete(m_pEffect);
+	}
+#endif // _DEBUG
+
+}
---
+++ JUSIN_8/9일차 Framework 221213/Engine/private/Model.cpp
@@ -0,0 +1,338 @@
+#include "..\public\Model.h"
+#include "Mesh.h"
+#include "Texture.h"
+#include "Shader.h"
+#include "Bone.h"
+#include "Animation.h"
+
+CModel::CModel(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CComponent(pDevice, pContext)
+{
+}
+
+CModel::CModel(const CModel & rhs)
+	: CComponent(rhs)
+	, m_pAIScene(rhs.m_pAIScene)
+	, m_eType(rhs.m_eType)
+	, m_iNumMeshes(rhs.m_iNumMeshes)
+	/*, m_Meshes(rhs.m_Meshes)*/
+	, m_Materials(rhs.m_Materials)
+	, m_iNumMaterials(rhs.m_iNumMaterials)
+	/*, m_Bones(rhs.m_Bones)*/
+	, m_iNumBones(rhs.m_iNumBones)
+	/*, m_Animations(rhs.m_Animations)*/
+	, m_iNumAnimations(rhs.m_iNumAnimations)
+	, m_iCurrentAnimIndex(rhs.m_iCurrentAnimIndex)
+	, m_PivotMatrix(rhs.m_PivotMatrix)
+{
+	/*for (auto& pBone : m_Bones)
+		Safe_AddRef(pBone);*/
+
+	//for (auto& pAnimation : m_Animations)
+	//	Safe_AddRef(pAnimation);
+
+	for (auto& Material : m_Materials)
+	{
+		for (_uint i = 0; i < AI_TEXTURE_TYPE_MAX; ++i)
+			Safe_AddRef(Material.pTexture[i]);
+	}
+
+	for (auto& pMesh : rhs.m_Meshes)
+	{
+		m_Meshes.push_back((CMesh*)pMesh->Clone());
+	}
+		
+
+}
+
+CBone * CModel::Get_BonePtr(const char * pBoneName)
+{
+	auto	iter = find_if(m_Bones.begin(), m_Bones.end(), [&](CBone* pBone)->_bool
+	{
+		return !strcmp(pBoneName, pBone->Get_Name());
+	});
+
+	if (iter == m_Bones.end())
+		return nullptr;
+
+	return *iter;
+
+
+	return nullptr;
+}
+
+HRESULT CModel::Initialize_Prototype(TYPE eType, const char * pModelFilePath, _fmatrix PivotMatrix)
+{
+	_uint			iFlag = 0;
+
+	if(TYPE_NONANIM == eType)
+		iFlag = aiProcess_PreTransformVertices | aiProcess_ConvertToLeftHanded | aiProcessPreset_TargetRealtime_Fast;
+	else
+		iFlag = aiProcess_ConvertToLeftHanded | aiProcessPreset_TargetRealtime_Fast;
+
+	m_eType = eType;
+
+	m_pAIScene = m_Importer.ReadFile(pModelFilePath, iFlag);
+	if (nullptr == m_pAIScene)
+		return E_FAIL;
+
+	XMStoreFloat4x4(&m_PivotMatrix, PivotMatrix);
+
+	/* 뼈를 로드한다. / */
+	/*if (FAILED(Ready_Bones(m_pAIScene->mRootNode, nullptr)))
+		return E_FAIL;*/
+
+	if (FAILED(Ready_MeshContainers()))
+		return E_FAIL;
+
+	if (FAILED(Ready_Materials(pModelFilePath)))
+		return E_FAIL;
+
+
+
+ 	return S_OK;
+}
+
+HRESULT CModel::Initialize(void * pArg)
+{
+	/* 모델의 전체뼈를 로드하여 보관한다. 메시별로 구분하지않는다. */
+	if (FAILED(Ready_Bones(m_pAIScene->mRootNode, nullptr)))
+		return E_FAIL;
+
+	/* 전체뼈를 보관하고 있느 ㄴm_Bones에서 각 메시에 영향을 주느 ㄴ뼈들을 찾아서. 
+	메시 안에 다시 재보관한다(참조) */
+	for (auto& pMesh : m_Meshes)
+	{
+		pMesh->SetUp_MeshBones(this);
+	}
+
+	/* 애니메이션을 구동하는데 필요한 뼈대를 찾는 작업이 이뤄진다. 
+	애니메이션을 구동하는데 필요한 뼈대 : 채널, 
+	특정 채널(뼈)에 대해서 현재 시간에 맞는 키프레임상태를 얻어오고, 이걸 행렬로 만들고. 
+	이 행렬을 전체뼈를 보관하고 있느 ㄴm_Bones중 같은 이름을 가진 뼈에 m_TransformMarix로 교체해주기 위해서. 
+	*/
+
+	if (FAILED(Ready_Animation()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+void CModel::Play_Animation(_double TimeDelta)
+{
+	if (TYPE_NONANIM == m_eType)
+		return;
+
+	/* 현재 애니메이션에 맞는 뼈들의 TranformMAtrix를 갱신하낟. */
+	m_Animations[m_iCurrentAnimIndex]->Update_Bones(TimeDelta);
+
+	for (auto& pBone : m_Bones)
+	{
+		if (nullptr != pBone)
+			pBone->Compute_CombindTransformationMatrix();
+	}
+	int a = 10;
+}
+
+HRESULT CModel::Bind_Material(CShader * pShader, _uint iMeshIndex, aiTextureType eType, const char * pConstantName)
+{
+	if (iMeshIndex >= m_iNumMeshes)
+		return E_FAIL;
+
+	_uint iMaterialIndex = m_Meshes[iMeshIndex]->Get_MaterialIndex();
+
+	if (iMaterialIndex >= m_iNumMaterials)
+		return E_FAIL;
+
+	if (nullptr != m_Materials[iMaterialIndex].pTexture[eType])
+	{
+		m_Materials[iMaterialIndex].pTexture[eType]->Bind_ShaderResource(pShader, pConstantName);
+	}
+	else	
+		return E_FAIL;	
+
+	return S_OK;
+}
+
+HRESULT CModel::Render(CShader* pShader, _uint iMeshIndex, const char* pBoneConstantName, _uint iPassIndex)
+{
+	if (nullptr != m_Meshes[iMeshIndex])
+	{
+		if (nullptr != pBoneConstantName)
+		{
+			_float4x4		BoneMatrices[128];
+
+			m_Meshes[iMeshIndex]->SetUp_BoneMatrices(BoneMatrices, XMLoadFloat4x4(&m_PivotMatrix));
+			
+			pShader->Set_MatrixArray(pBoneConstantName, BoneMatrices, 128);
+		}		
+
+		pShader->Begin(iPassIndex);
+
+		m_Meshes[iMeshIndex]->Render();
+	}
+
+	return S_OK;
+}
+
+HRESULT CModel::Ready_Bones(aiNode * pAINode, CBone* pParent)
+{
+	/* 생성하고자해ㅑㅆ떤 뼈의 정보. */
+	CBone*		pBone = CBone::Create(pAINode, pParent);
+	if (nullptr == pBone)
+		return E_FAIL;
+
+	m_Bones.push_back(pBone);
+
+	/* 이뼈의 자식뼈를 만든다.  */
+	for (_uint i = 0; i < pAINode->mNumChildren; ++i)
+	{
+		Ready_Bones(pAINode->mChildren[i], pBone);
+	}
+
+	return S_OK;
+}
+
+HRESULT CModel::Ready_MeshContainers()
+{
+	if (nullptr == m_pAIScene)
+		return E_FAIL;
+
+	m_iNumMeshes = m_pAIScene->mNumMeshes;
+
+	for (_uint i = 0; i < m_iNumMeshes; ++i)
+	{
+		aiMesh*		pAIMesh = m_pAIScene->mMeshes[i];
+
+		CMesh*		pMesh = CMesh::Create(m_pDevice, m_pContext, m_eType, pAIMesh, this);
+		if (nullptr == pMesh)
+			return E_FAIL;
+
+		m_Meshes.push_back(pMesh);
+	}	
+
+	return S_OK;
+}
+
+HRESULT CModel::Ready_Materials(const char* pModelFilePath)
+{
+	char		szDirectory[MAX_PATH] = "";
+
+	_splitpath_s(pModelFilePath, nullptr, 0, szDirectory, MAX_PATH, nullptr, 0, nullptr, 0);
+
+	/* 텍스쳐에 기록되어있는 픽셀단위 재질 정보를 로드한다. */
+	m_iNumMaterials = m_pAIScene->mNumMaterials;
+
+	for (_uint i = 0; i < m_iNumMaterials; ++i)
+	{
+		char		szTextureFileName[MAX_PATH] = "";
+		char		szExt[MAX_PATH] = "";
+		char		szTexturePath[MAX_PATH] = "";
+
+		MODELMATERIAL			ModelMaterial;
+		ZeroMemory(&ModelMaterial, sizeof(ModelMaterial));
+
+		aiMaterial*		pAIMaterial = m_pAIScene->mMaterials[i];
+
+		for (_uint j = 0; j < AI_TEXTURE_TYPE_MAX; ++j)
+		{			
+			/*j == 0 = > aiTextureType_NONE;
+			j == 1 = > aiTextureType_DIFFUSE;
+			j == 2 = > aiTextureType_SPECULAR;*/
+
+			aiString			strTexturePath;			
+			
+			if (FAILED(pAIMaterial->GetTexture(aiTextureType(j), 0, &strTexturePath)))
+				continue;
+
+			_splitpath_s(strTexturePath.data, nullptr, 0, nullptr, 0, szTextureFileName, MAX_PATH, szExt, MAX_PATH);
+
+			strcpy_s(szTexturePath, szDirectory);
+			strcat_s(szTexturePath, szTextureFileName);
+			strcat_s(szTexturePath, szExt);
+
+			_tchar			szFullPath[MAX_PATH] = TEXT("");
+
+			MultiByteToWideChar(CP_ACP, 0, szTexturePath, strlen(szTexturePath), szFullPath, MAX_PATH);
+			
+			ModelMaterial.pTexture[j] = CTexture::Create(m_pDevice, m_pContext, szFullPath);
+
+			if (nullptr == ModelMaterial.pTexture[j])
+				return E_FAIL;
+		}
+
+		m_Materials.push_back(ModelMaterial);
+	}
+
+	return S_OK;
+}
+
+HRESULT CModel::Ready_Animation()
+{
+	m_iNumAnimations = m_pAIScene->mNumAnimations;
+
+	for (_uint i = 0; i < m_iNumAnimations; ++i)
+	{
+		aiAnimation*		pAIAnimation = m_pAIScene->mAnimations[i];
+
+		CAnimation*			pAnim = CAnimation::Create(pAIAnimation, this);
+		if (nullptr == pAnim)
+			return E_FAIL;
+
+		m_Animations.push_back(pAnim);
+	}
+
+	return S_OK;
+}
+
+CModel * CModel::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext, TYPE eType, const char * pModelFilePath, _fmatrix PivotMatrix)
+{
+	CModel*		pInstance = new CModel(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype(eType, pModelFilePath, PivotMatrix)))
+	{
+		MSG_BOX("Failed to Created : CModel");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+CComponent * CModel::Clone(void * pArg)
+{
+	CModel*		pInstance = new CModel(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Cloned : CModel");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CModel::Free()
+{
+	__super::Free();
+
+	for (auto& pAnimation : m_Animations)
+		Safe_Release(pAnimation);
+	m_Animations.clear();
+
+	for (auto& pBone : m_Bones)
+		Safe_Release(pBone);
+
+	m_Bones.clear();
+
+	for (auto& Material : m_Materials)
+	{
+		for (_uint i = 0; i < AI_TEXTURE_TYPE_MAX; ++i)
+			Safe_Release(Material.pTexture[i]);
+	}
+	m_Materials.clear();
+
+	for (auto& pMesh : m_Meshes)
+		Safe_Release(pMesh);
+	m_Meshes.clear();
+
+	m_Importer.FreeScene();
+}
