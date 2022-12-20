a4e9cc915d116494988b83d393a76163ff8719fa
SuHyeonOk <tntngus1223@naver.com>
2022-12-20 16:14:28 +0900

    8_14 Instancing

---
+++ JUSIN_8/14일차 - Framework 221220/Client/public/Weapon.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CCollider;
+class CShader;
+class CRenderer;
+class CModel;
+class CBone;
+END
+
+BEGIN(Client)
+
+class CWeapon final : public CGameObject
+{
+public:
+	typedef struct tagWeaponDesc
+	{
+		_float4x4			PivotMatrix;
+		CBone*				pSocket;
+		CTransform*			pTargetTransform;
+	}WEAPONDESC;
+
+private:
+	CWeapon(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CWeapon(const CWeapon& rhs);
+	virtual ~CWeapon() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Tick(_double TimeDelta) override;
+	virtual void Late_Tick(_double TimeDelta) override;
+	virtual HRESULT Render() override;
+
+private:
+	CShader*				m_pShaderCom = nullptr;
+	CRenderer*				m_pRendererCom = nullptr;
+	CModel*					m_pModelCom = nullptr;
+	CCollider*				m_pColliderCom = nullptr;
+
+	WEAPONDESC				m_WeaponDesc;
+
+	_float4x4				m_SocketMatrix;
+
+	
+
+private:
+	HRESULT SetUp_Components();
+	HRESULT SetUp_ShaderResources();
+
+public:
+	static CWeapon* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg = nullptr) override;
+	virtual void Free() override;
+};
+
+END
