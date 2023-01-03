8cf83c5dff0b48ccc20da0c3f2150809b72a50bf
SuHyeonOk <tntngus1223@naver.com>
2023-01-03 15:57:51 +0900

    8_19 truncated curling

---
+++ JUSIN_8/19일차 - Framework 230103/Engine/private/Animation.cpp
@@ -0,0 +1,82 @@
+#include "..\public\Animation.h"
+#include "Channel.h"
+
+CAnimation::CAnimation()
+	: m_isLooping(true)
+{
+}
+
+HRESULT CAnimation::Initialize(aiAnimation * pAIAnimation, CModel* pModel)
+{
+	strcpy_s(m_szName, pAIAnimation->mName.data);
+
+	m_Duration = pAIAnimation->mDuration;
+
+	m_TickPerSecond = pAIAnimation->mTicksPerSecond;
+
+	/* 이 애니메이션 구동하는데 필요한 뼈대의 갯수다.  */
+	m_iNumChannels = pAIAnimation->mNumChannels;
+
+	for (_uint i = 0; i < m_iNumChannels; ++i)
+	{
+		aiNodeAnim*		pAINodeAnim = pAIAnimation->mChannels[i];
+
+		CChannel*		pChannel = CChannel::Create(pAINodeAnim, pModel);
+		if (nullptr == pChannel)
+			return E_FAIL;
+
+		m_Channels.push_back(pChannel);
+	}
+
+	return S_OK;
+}
+
+void CAnimation::Update_Bones(_double TimeDelta)
+{
+	if (true == m_isFinished &&
+		false == m_isLooping)
+	{
+		return;
+	}
+
+	m_PlayTime += m_TickPerSecond * TimeDelta;
+
+	if (m_PlayTime >= m_Duration)
+	{
+		m_PlayTime = 0.0;
+		m_isFinished = true;
+	}
+
+	for (_uint i = 0; i < m_iNumChannels; ++i)
+	{
+		if (true == m_isFinished)
+			m_Channels[i]->Reset_KeyFrameIndex();
+
+		m_Channels[i]->Update_TransformMatrix(m_PlayTime);
+	}
+
+	if (true == m_isFinished)
+		m_isFinished = false;
+
+}
+
+CAnimation * CAnimation::Create(aiAnimation * pAIAnimation, CModel* pModel)
+{
+	CAnimation*		pInstance = new CAnimation();
+
+	if (FAILED(pInstance->Initialize(pAIAnimation, pModel)))
+	{
+		MSG_BOX("Failed to Created : CAnimation");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CAnimation::Free()
+{
+	for (auto& pChannel : m_Channels)
+		Safe_Release(pChannel);
+
+	m_Channels.clear();
+}
+
---
+++ JUSIN_8/19일차 - Framework 230103/Engine/public/GameInstance.h
@@ -0,0 +1,104 @@
+#pragma once
+
+/* 싱글톤.  */
+/* 클라이언트와 엔진의 소통을 위해 만들어진 클래스. */
+
+#include "Base.h"
+#include "Input_Device.h"
+#include "Component_Manager.h"
+#include "PipeLine.h"
+
+
+BEGIN(Engine)
+
+class ENGINE_DLL CGameInstance final : public CBase
+{
+	DECLARE_SINGLETON(CGameInstance);
+private:
+	CGameInstance();
+	virtual ~CGameInstance() = default;
+
+public:
+	static _uint Get_StaticLevelIndex() {
+		return m_iStaticLevelIndex;
+	}
+
+public: /* For.GameInstance */	
+	static const _tchar*			m_pPrototypeTransformTag;
+
+public: /* For.GameInstance */
+	HRESULT Initialize_Engine(HINSTANCE hInst, _uint iNumLevels, const GRAPHIC_DESC& GraphicDesc, ID3D11Device** ppDeviceOut, ID3D11DeviceContext** ppContextOut);
+	void Tick_Engine(_double TimeDelta);
+	void Clear_Level(_uint iLevelIndex);
+
+public: /* For.Graphic_Device */
+	HRESULT Clear_Graphic_Device(const _float4* pColor);
+	HRESULT Present();
+
+public: /* For.Input_Device */
+	_byte		Get_DIKeyState(_ubyte byKeyID);
+	_byte		Get_DIMouseState(CInput_Device::MOUSEKEYSTATE byMouseID);
+	_long		Get_DIMouseMove(CInput_Device::MOUSEMOVESTATE eMoveState);
+
+public: /* For.Level_Manager */
+	HRESULT Open_Level(_uint iLevelIndex, class CLevel* pNewLevel);	
+	HRESULT Render_Level();
+
+public: /* For.Object_Manager */
+	class CComponent* Get_ComponentPtr(_uint iLevelIndex, const _tchar* pLayerTag, const _tchar* pComponentTag, _uint iIndex = 0);
+	HRESULT Add_Prototype(const _tchar* pPrototypeTag, class CGameObject* pPrototype);
+	HRESULT Clone_GameObject(_uint iLevelIndex, const _tchar* pLayerTag, const _tchar* pPrototypeTag, void* pArg = nullptr);
+	CGameObject* Clone_GameObject(const _tchar* pPrototypeTag, void* pArg = nullptr);
+	
+public: /* For.Component_Manager */
+	HRESULT Add_Prototype(_uint iLevelIndex, const _tchar* pPrototypeTag, class CComponent* pPrototype);
+	class CComponent* Clone_Component(_uint iLevelIndex, const _tchar* pPrototypeTag, void* pArg = nullptr);
+
+public: /* For.PipeLine */
+	_matrix Get_TransformMatrix(CPipeLine::TRANSFORMSTATE eState) ;
+	_float4x4 Get_TransformFloat4x4(CPipeLine::TRANSFORMSTATE eState) ;
+	_matrix Get_TransformMatrix_Inverse(CPipeLine::TRANSFORMSTATE eState) ;
+	void Set_Transform(CPipeLine::TRANSFORMSTATE eState, _fmatrix TransformMatrix);
+	_float4 Get_CamPosition();
+
+public: /* For.Timer_Manager */ 
+	_double		Get_TimeDelta(const _tchar* pTimerTag);
+	HRESULT		Ready_Timer(const _tchar* pTimerTag);
+	void		Update_Timer(const _tchar* pTimerTag);
+
+public: /* For.Light_Manager */ 
+	const LIGHTDESC* Get_LightDesc(_uint iIndex);
+	HRESULT Add_Light(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const LIGHTDESC& LightDesc);
+
+public: /* For.Font_Manager */
+	HRESULT Add_Font(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const _tchar* pFontTag, const _tchar* pFontFilePath);
+	HRESULT Render_Font(const _tchar* pFontTag, const _tchar* pText, const _float2& vPos, _float fRadian, _float2 vScale, _fvector vColor = XMVectorSet(1.f, 1.f, 1.f, 1.f));
+
+public: /* For.Frustum */
+	_bool isInFrustum_WorldSpace(_fvector vWorldPos, _float fRange = 0.f);
+	_bool isInFrustum_LocalSpace(_fvector vLocalPos, _float fRange = 0.f);
+
+private:
+	static _uint					m_iStaticLevelIndex;
+
+private:	
+	class CGraphic_Device*			m_pGraphic_Device = nullptr;
+	class CInput_Device*			m_pInput_Device = nullptr;
+	class CLevel_Manager*			m_pLevel_Manager = nullptr;
+	class CObject_Manager*			m_pObject_Manager = nullptr;
+	class CComponent_Manager*		m_pComponent_Manager = nullptr;
+	class CPipeLine*				m_pPipeLine = nullptr;
+	class CTimer_Manager*			m_pTimer_Manager = nullptr;
+	class CLight_Manager*			m_pLight_Manager = nullptr;
+	class CFont_Manager*			m_pFont_Manager = nullptr;
+	class CFrustum*					m_pFrustum = nullptr;
+
+
+public:
+	static void Release_Engine();
+
+public:
+	virtual void Free() override;
+};
+
+END
