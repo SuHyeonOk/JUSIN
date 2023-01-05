cfcd30d8eb2a3b6021e64c1731e0de0152f6ae17
SuHyeonOk <tntngus1223@naver.com>
2023-01-05 12:40:46 +0900

    9_1 a crack, Deepard Shader

---
+++ JUSIN_9/1일차 - Framework 230105/Framework/Client/public/Monster.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CShader;
+class CRenderer;
+class CModel;
+class CCollider;
+END
+
+BEGIN(Client)
+
+class CMonster final : public CGameObject
+{
+public:
+	enum COLLIDERTYPE { COLLTYPE_AABB, COLLTYPE_OBB, COLLTYPE_SPHERE, COLLTYPE_END };
+private:
+	CMonster(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CMonster(const CMonster& rhs);
+	virtual ~CMonster() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Tick(_double TimeDelta) override;
+	virtual void Late_Tick(_double TimeDelta) override;
+	virtual HRESULT Render() override;
+
+private:
+	CShader*				m_pShaderCom = nullptr;
+	CRenderer*				m_pRendererCom = nullptr;
+	CModel*					m_pModelCom = nullptr;
+	CCollider*				m_pColliderCom[COLLTYPE_END] = { nullptr };
+
+private:
+	HRESULT SetUp_Components();
+	HRESULT SetUp_ShaderResources();
+
+private:
+	void Collision_ToPlayer();
+
+public:
+	static CMonster* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg = nullptr) override;
+	virtual void Free() override;
+};
+
+END
