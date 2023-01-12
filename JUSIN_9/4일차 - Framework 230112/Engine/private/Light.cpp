9414b02aaa341115e571685b5f5e5c01bb9306a8
SuHyeonOk <tntngus1223@naver.com>
2023-01-12 14:30:49 +0900

    PP_ 9_4 Drawing Shader, Drawing Back Buffer

---
+++ JUSIN_9/4일차 - Framework 230112/Engine/private/Light.cpp
@@ -0,0 +1,49 @@
+#include "..\public\Light.h"
+#include "Shader.h"
+#include "VIBuffer_Rect.h"
+
+CLight::CLight(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: m_pDevice(pDevice)
+	, m_pContext(pContext)
+{
+	Safe_AddRef(m_pDevice);
+	Safe_AddRef(m_pContext);
+}
+
+HRESULT CLight::Initialize(const LIGHTDESC & LightDesc)
+{
+	m_LightDesc = LightDesc;
+
+	return S_OK;
+}
+
+HRESULT CLight::Render(CVIBuffer_Rect * pVIBuffer, CShader * pShader)
+{
+	if (FAILED(pShader->Set_RawValue("g_vLightDir", &m_LightDesc.vDirection, sizeof(_float4))))
+		return E_FAIL;
+
+	pShader->Begin(1);
+
+	pVIBuffer->Render();
+
+	return S_OK;
+}
+
+CLight * CLight::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext, const LIGHTDESC & LightDesc)
+{
+	CLight*		pInstance = new CLight(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize(LightDesc)))
+	{
+		MSG_BOX("CLight Create Failed");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CLight::Free()
+{
+	Safe_Release(m_pDevice);
+	Safe_Release(m_pContext);
+}
