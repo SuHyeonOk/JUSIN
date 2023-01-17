0d617b632ea2c21723061fac5f3e83aa9d9dded3
SuHyeonOk <tntngus1223@naver.com>
2023-01-17 13:51:28 +0900

    9_6 Point Shader

---
+++ JUSIN_9/6일차 - Framework 230117/Framework/Engine/public/Layer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Base.h"
+
+BEGIN(Engine)
+
+class CLayer final : public CBase
+{
+private:
+	CLayer();
+	virtual ~CLayer() = default;
+
+public:
+	class CComponent* Get_ComponentPtr(const _tchar* pComponentTag, _uint iIndex);
+
+public:
+	HRESULT Add_GameObject(class CGameObject* pGameObject);
+	HRESULT Initialize();
+	void Tick(_double TimeDelta);
+	void Late_Tick(_double TimeDelta);
+	
+	
+
+private:
+	list<class CGameObject*>			m_GameObjects;
+	typedef list<class CGameObject*>	GAMEOBJECTS;
+
+public:
+	static CLayer* Create();
+	virtual void Free() override;
+
+};
+
+END
